aa1a9b16ccdc0b2217b6681f376b147874b9ae94
creativenucleus <james+github@creativenucleus.com>
2015-09-17 12:21:00 +0100

    First commit

---
+++ hashcheck.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "io"
+    "fmt"
+    "os"
+    "github.com/codegangsta/cli"
+    "crypto/md5"
+)
+
+func main() {
+    app := cli.NewApp()
+    app.Name = "hashcheck"
+    app.Usage = "Get the MD5 hash of a file"
+    app.Commands = []cli.Command{
+        {
+            Name: "get",
+            Usage: "Get the hash",
+            Action: cliActionGet(),
+        },
+    }
+      
+    app.Run(os.Args)
+}
+
+
+func cliActionGet() (func(*cli.Context)) {
+    return func(c *cli.Context) {
+        filepath := c.Args().First()
+        if(filepath == "") {
+            fmt.Println("Please supply a file")
+            return
+        }
+        
+        file, err := os.Open(filepath)
+        if err != nil {
+            fmt.Println("Could not open the file")
+        }
+        defer file.Close()
+
+        var result []byte
+        hash := md5.New()
+        if _, err := io.Copy(hash, file); err != nil {
+            return
+        }
+                       
+        fmt.Printf("MD5 %x", hash.Sum(result))
+    }
+}
